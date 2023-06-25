1d335132d1a561845eb13d996518e40434a21be1
glushevskaya <ls.glushevskaya@tensor.ru>
2023-06-25 13:43:03 +0300

    Задание 11

--- lection 11/task_1.py
+++ lection 11/task_1.py
@@ -9,3 +9,44 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+try:
+    #  Заходим на сайт 'https://sbis.ru/'
+    driver.get("https://sbis.ru/")
+    driver.maximize_window()
+    assert driver.current_url == "https://sbis.ru/", 'Не верно открыт сайт'
+
+    #  Переходим в раздел контакты, проверяем адрес и заголовок страницы
+    button_txt = "Контакты"
+    contacts_btn = driver.find_element(By.XPATH, "//a[text()='Контакты']")
+    assert contacts_btn.is_displayed(), "Вкладка Контакты не отображается на странице"
+    assert contacts_btn.text == button_txt, "Неверный текст вкладки Контакты"
+    assert contacts_btn.get_attribute("href") == "https://sbis.ru/contacts", "Неверная ссылка у вкладки Контакты"
+    contacts_btn.click()
+    assert "https://sbis.ru/contacts" in driver.current_url
+    sleep(1)
+
+    # Находим баннер Тензора, проверяем его наличие, нажимаем
+    banner_tensor = driver.find_element(By.CSS_SELECTOR, "[title='tensor.ru']")
+    assert banner_tensor.is_displayed(), "Баннер Тензор не отображается"
+    banner_tensor.click()
+    driver.switch_to.window(driver.window_handles[1])
+    assert driver.current_url == 'https://tensor.ru/', 'Неверный адрес страницы'
+
+    # Проверяем, что есть блок "Сила в людях"
+    block_tensor = driver.find_element(By.CSS_SELECTOR, ".tensor_ru-Index__card-title")
+    driver.execute_script("return arguments[0].scrollIntoView(true);", block_tensor)
+    assert block_tensor.is_displayed(), "Блок новости 'Сила в людях' не отображается"
+
+    # Переходим в 'Подробнее' и убеждаемся, что открывается https://tensor.ru/about"
+    more = driver.find_element(By.CSS_SELECTOR, ".tensor_ru-link.tensor_ru-Index__link[href='/about']")
+    driver.execute_script("arguments[0].scrollIntoView();", more)
+    more.click()
+    assert driver.current_url == "https://tensor.ru/about", "Неправильный адрес сайта"
+
+finally:
+    driver.quit()
--- lection 11/task_2.py
+++ lection 11/task_2.py
@@ -7,4 +7,56 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
+from selenium.webdriver import Keys, ActionChains
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+login, password = 'glushevskaya1', 'glushevskaya1glushevskaya1'
+user = 'Глушевская Лидия Сергеевна'
+text = 'Вебинар 11 задание 2 - отправка и удаление сообщения'
+
+try:
+    # Авторизация на https://fix-online.sbis.ru/
+    driver.get("https://fix-online.sbis.ru/")
+    sleep(2)
+    driver.find_element(By.NAME, 'Login').send_keys(login, Keys.ENTER)
+    sleep(1)
+    driver.find_element(By.NAME, 'Password').send_keys(password, Keys.ENTER)
+    sleep(5)
+
+    # Открытие раздела контакты
+    driver.find_element(By.CSS_SELECTOR, '[data-qa="NavigationPanels-Accordion__title"]').click()
+    sleep(2)
+    driver.find_element(By.CSS_SELECTOR, '[name="headTitle"]').click()
+    sleep(2)
+
+    # Отправка сообщения самому себе
+    driver.find_element(By.CSS_SELECTOR, '.icon-RoundPlus').click()
+    sleep(2)
+    driver.find_element(By.CLASS_NAME, 'controls-Search__nativeField_caretEmpty').send_keys(user, Keys.ENTER)
+    sleep(2)
+    driver.find_element(By.CSS_SELECTOR, '[data-qa="person-Information__fio"]').click()
+    sleep(2)
+    driver.find_element(By.CSS_SELECTOR, '.textEditor_Viewer__Paragraph').send_keys(text)
+    sleep(2)
+    driver.find_element(By.CSS_SELECTOR, '.icon-BtArrow').click()
+    sleep(2)
+
+    # Проверка наличия сообщения
+    message = driver.find_element(By.CSS_SELECTOR, '.msg-entity-text')
+    sleep(2)
+    assert message.text == text, 'Сообщение не найдено'
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(message)
+    action_chains.context_click(message)
+    action_chains.perform()
+
+    # Удаление сообщения и проверка удаления
+    driver.find_element(By.CSS_SELECTOR, '[title="Перенести в удаленные"]').click()
+    sleep(2)
+    assert driver.find_elements(By.CSS_SELECTOR, '.msg-dialogs-item__content-inner') != message, 'Сообщение не удалено!'
+    sleep(2)
+
+finally:
+    driver.quit()
